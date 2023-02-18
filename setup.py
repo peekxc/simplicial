36a832ea6c648abe9810b767f1cc16764314f3d7
peekxc <matt.piekenbrock@gmail.com>
2023-02-18 15:25:21 +0000

    CI changes

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ from setuptools import setup, Extension, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 # flags = distutils.sysconfig.get_config_var("CFLAGS")
-
+print("COMPILER: ", os.environ["CXX"])
+# os.environ["CXX"] = "g++"
 base_path = os.path.dirname(__file__)
-compile_flags = list(dict.fromkeys(sysconfig.get_config_var('CFLAGS').split()))
-compile_flags += list(dict.fromkeys(sysconfig.get_config_var('CPPFLAGS').split()))
-compile_flags += ["-std=c++17", "-Wall", "-Wextra", "-O2", "-Wno-unused-parameter"]
+# compile_flags = list(dict.fromkeys(sysconfig.get_config_var('CFLAGS').split()))
+# compile_flags += list(dict.fromkeys(sysconfig.get_config_var('CPPFLAGS').split()))
+compile_flags = ["-std=c++17", "-Wall", "-Wextra", "-O2", "-Wno-unused-parameter"]
+# compile_flags = ["-I/opt/conda/include/python3.10", "-I/opt/conda/include/python3.10", "-Wno-unused-result", "-Wsign-compare", "-march=nocona", "-mtune=haswell", "-ftree-vectorize", "-fPIC", "-fstack-protector-strong", "-fno-plt", "-O3", "-ffunction-sections", "-pipe", "-isystem", "/opt/conda/include", "-fdebug-prefix-map=/croot/python-split_1669298683653/work=/usr/local/src/conda/python-3.10.8", "-fdebug-prefix-map=/opt/conda=/usr/local/src/conda-prefix", "-fuse-linker-plugin", "-ffat-lto-objects", "-flto-partition=none", "-flto", "-DNDEBUG", "-fwrapv", "-O3", "-Wall", "-L/opt/conda/lib/python3.10/config-3.10-x86_64-linux-gnu", "-L/opt/conda/lib", "-lcrypt", "-lpthread", "-ldl", "-lutil", "-lm", "-lm"]
+# "-lstdc++", "-lpython3.9","-L/opt/conda/lib/python3.10/config-3.10-x86_64-linux-gnu", "-L/opt/conda/lib", "-lcrypt", "-lpthread", "-ldl",  "-lutil", "-lm", "-ld"]
 print(f"COMPILER FLAGS: { str(compile_flags) }")
 
 ext_modules = [
